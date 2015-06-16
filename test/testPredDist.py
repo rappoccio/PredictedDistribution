f5687fa79876e86ace2ab0ec9533d401d8a1cae6
Salvatore Rappoccio <rappoccio@gmail.com>
2015-06-16 09:13:47 -0500

    Updating testPredDist to new directory

--- test/testPredDist.py
+++ test/testPredDist.py
@@ -1,10 +1,5 @@
 
 import ROOT
-ROOT.gROOT.Macro("rootlogon.C")
-
-
-ROOT.gSystem.Load("libAnalysisTTBSMTuplesAnalysis")
-
-from Analysis.TTBSMTuplesAnalysis import *
+ROOT.gSystem.Load("libAnalysisPredictedDistribution")
 a = ROOT.TH1D("a", "a", 10, 0, 10)
 pred = ROOT.PredictedDistribution(a, "bla", "bla", 10, 0, 10)
